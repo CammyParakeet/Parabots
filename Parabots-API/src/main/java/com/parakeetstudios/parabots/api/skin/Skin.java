ca1cfd4081dcccc08f1a9fe9e037ad4f42e75f86
SheeshSpace <cammy.sheesh@gmail.com>
2023-08-08 13:12:24 +1200

    EnumSet for different skinparts

--- Parabots-API/src/main/java/com/parakeetstudios/parabots/api/skin/Skin.java
+++ Parabots-API/src/main/java/com/parakeetstudios/parabots/api/skin/Skin.java
@@ -3,6 +3,7 @@ package com.parakeetstudios.parabots.api.skin;
 import com.parakeetstudios.parabots.api.bot.Parabot;
 import org.bukkit.entity.Player;
 
+import java.util.EnumSet;
 import java.util.UUID;
 
 public interface Skin {
@@ -10,10 +11,22 @@ public interface Skin {
     UUID getSkinID();
 
     String getTexture();
-
     String getSignature();
 
     void applyTo(Parabot bot);
     void applyTo(Player player);
+
+    EnumSet<SkinPart> getVisibleSkinParts();
+    void setVisibleSkinParts(EnumSet<SkinPart> parts);
+
+    public enum SkinPart {
+        CAPE,
+        JACKET,
+        LEFT_SLEEVE,
+        RIGHT_SLEEVE,
+        LEFT_PANT_LEG,
+        RIGHT_PANT_LEG,
+        HAT
+    }
 
 }
